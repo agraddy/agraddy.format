8f05b1b628502e2881b8233286f1bcf55687cd20
Anthony Graddy <anthony@locationq.com>
2017-12-17 09:24:13 +0000

    Format money

--- test/index.js
+++ test/index.js
@@ -4,6 +4,11 @@ var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
-
-
+tap.assert.equal(mod.money('1'), '$1.00', 'Should be equal.');
+tap.assert.equal(mod.money(1), '$1.00', 'Should be equal.');
+tap.assert.equal(mod.money('1', true), '$1', 'Should be equal.');
+tap.assert.equal(mod.money(1, true), '$1', 'Should be equal.');
+tap.assert.equal(mod.money(1000), '$1,000.00', 'Should be equal.');
+tap.assert.equal(mod.money('1,000'), '$1,000.00', 'Should be equal.');
+tap.assert.equal(mod.money('$1,000'), '$1,000.00', 'Should be equal.');
+tap.assert.equal(mod.money('asdf') instanceof Error, true, 'Should create an error when not a valid number.');
